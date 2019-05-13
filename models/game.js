ceb322fbd93ced487f44824ff6c16b84c22dee13
Kieran Rigby <kieran@kierigby.com>
2019-05-13 02:57:39 +0100

    Less debug info

--- models/game.js
+++ models/game.js
@@ -134,7 +134,7 @@ class Game{
     this.radius = geoData.radius;
     this.center = geoData.center;
     console.log("Starting game + " +  this.id);
-    console.log("Users:" + io.sockets.clients(this.id))
+    // console.log("Users:" + io.sockets.clients(this.id))
     //emit to the room that the game has started
     io.to(this.id).emit('gameStarted',{message: "Game Starting", game:Game.cleanGame(this)});
 
